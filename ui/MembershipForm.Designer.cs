8d05eb81306fbffc2a171dd62074e4c29e6869d1
prashantha.konkodi <prashantha.konkodi@accenture.com>
2020-10-24 09:21:09 -0400

    Membership form layout

--- ui/MembershipForm.Designer.cs
+++ ui/MembershipForm.Designer.cs
@@ -28,16 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MembershipForm));
             this.label1 = new System.Windows.Forms.Label();
             this.serialNoLb = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.dateValue = new System.Windows.Forms.Label();
             this.nameLbl = new System.Windows.Forms.Label();
-            this.nameTxt = new System.Windows.Forms.TextBox();
             this.phoneNoTxt = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.mobileNoLbl = new System.Windows.Forms.Label();
             this.mobileNoTxt = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.dobLbl = new System.Windows.Forms.Label();
@@ -49,7 +48,21 @@
             this.regular = new System.Windows.Forms.RadioButton();
             this.lifetimeBtn = new System.Windows.Forms.RadioButton();
             this.label5 = new System.Windows.Forms.Label();
+            this.nicknameTxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.commentTxt = new System.Windows.Forms.RichTextBox();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.nameTxt = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -78,12 +91,6 @@
             resources.ApplyResources(this.nameLbl, "nameLbl");
             this.nameLbl.Name = "nameLbl";
             // 
-            // nameTxt
-            // 
-            resources.ApplyResources(this.nameTxt, "nameTxt");
-            this.nameTxt.ForeColor = System.Drawing.Color.Black;
-            this.nameTxt.Name = "nameTxt";
-            // 
             // phoneNoTxt
             // 
             resources.ApplyResources(this.phoneNoTxt, "phoneNoTxt");
@@ -94,11 +101,6 @@
             // 
             resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-            // 
-            // mobileNoLbl
-            // 
-            resources.ApplyResources(this.mobileNoLbl, "mobileNoLbl");
-            this.mobileNoLbl.Name = "mobileNoLbl";
             // 
             // mobileNoTxt
             // 
@@ -159,6 +161,7 @@
             // lifetimeBtn
             // 
             resources.ApplyResources(this.lifetimeBtn, "lifetimeBtn");
+            this.lifetimeBtn.Checked = true;
             this.lifetimeBtn.Name = "lifetimeBtn";
             this.lifetimeBtn.TabStop = true;
             this.lifetimeBtn.UseVisualStyleBackColor = true;
@@ -168,11 +171,95 @@
             resources.ApplyResources(this.label5, "label5");
             this.label5.Name = "label5";
             // 
+            // nicknameTxt
+            // 
+            resources.ApplyResources(this.nicknameTxt, "nicknameTxt");
+            this.nicknameTxt.ForeColor = System.Drawing.Color.Black;
+            this.nicknameTxt.Name = "nicknameTxt";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // contextMenuStrip1
+            // 
+            resources.ApplyResources(this.contextMenuStrip1, "contextMenuStrip1");
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            // 
+            // commentTxt
+            // 
+            resources.ApplyResources(this.commentTxt, "commentTxt");
+            this.commentTxt.Name = "commentTxt";
+            // 
+            // cancelBtn
+            // 
+            resources.ApplyResources(this.cancelBtn, "cancelBtn");
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // saveBtn
+            // 
+            resources.ApplyResources(this.saveBtn, "saveBtn");
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.ForeColor = System.Drawing.Color.Black;
+            this.textBox2.Name = "textBox2";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Name = "textBox1";
+            // 
+            // nameTxt
+            // 
+            resources.ApplyResources(this.nameTxt, "nameTxt");
+            this.nameTxt.ForeColor = System.Drawing.Color.Black;
+            this.nameTxt.Name = "nameTxt";
+            // 
             // MembershipForm
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ControlBox = false;
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.commentTxt);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.nicknameTxt);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.serialNoTxt);
@@ -182,7 +269,6 @@
             this.Controls.Add(this.emailLbl);
             this.Controls.Add(this.mobileNoTxt);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.mobileNoLbl);
             this.Controls.Add(this.phoneNoTxt);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.nameTxt);
@@ -193,8 +279,11 @@
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MembershipForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -206,10 +295,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label dateValue;
         private System.Windows.Forms.Label nameLbl;
-        private System.Windows.Forms.TextBox nameTxt;
         private System.Windows.Forms.TextBox phoneNoTxt;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label mobileNoLbl;
         private System.Windows.Forms.TextBox mobileNoTxt;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label dobLbl;
@@ -221,5 +308,18 @@
         private System.Windows.Forms.RadioButton regular;
         private System.Windows.Forms.RadioButton lifetimeBtn;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox nicknameTxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox commentTxt;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox nameTxt;
     }
 }
