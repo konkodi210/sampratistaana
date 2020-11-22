d6b77e5d3b8fb16d7e42b28ab8efb4d5e537b845
prashantha <prashantha@konkodi>
2020-11-22 09:21:40 -0500

    Removed unwanted import

--- sampratistaana-javafx/src/main/java/org/sampratistaana/Messages.java
+++ sampratistaana-javafx/src/main/java/org/sampratistaana/Messages.java
@@ -1,9 +1,7 @@
 package org.sampratistaana;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
